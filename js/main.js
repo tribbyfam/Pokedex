c2a257efb51543854e3d5259a7100657712d4431
Mac <mac@MacBook-Pro.fios-router.home>
2018-08-18 15:21:05 -0500

    made mockup for first and second page layout

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
+// all initializers for materialize
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, 'isFixed');
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems, 'responsiveThreshold', 100);
+});
 
 
-axios.get('https://www.redbullshopus.com/products.json')
-.then(function (response) {
-  let final = response.data.products;
+
+
+
+
+
+// axios.get('https://www.redbullshopus.com/products.json')
+// .then(function (response) {
+//   let final = response.data.products;
 
   
 
 
-})
-.catch(function (error) {
-  console.log(error);
-});
+// })
+// .catch(function (error) {
+//   console.log(error);
+// });
