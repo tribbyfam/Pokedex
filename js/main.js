4f6a7e6e04abe81290a6c58a6fd62e906d2ecc73
Mac <mac@MacBook-Pro.fios-router.home>
2018-08-19 10:01:41 -0500

    first api calls for the poke charactristics table entry

--- js/main.js
+++ js/main.js
@@ -17,19 +17,43 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Slider.init(elems, 'duration', 1500);
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, 'accordion');
+});
 
 
 
 
+// class blueKetchum {
 
-// axios.get('https://www.redbullshopus.com/products.json')
-// .then(function (response) {
-//   let final = response.data.products;
+// }
 
-  
+// api calls requesting data
+
+let pokeFirstName = document.getElementById("poke-name");
+let pokeHpStats = document.getElementById("poke-hp");
+let pokeAttackStats = document.getElementById("poke-attack");
+let pokeDefenceStats = document.getElementById("poke-defence");
 
 
-// })
-// .catch(function (error) {
-//   console.log(error);
-// });
+axios.get('http://fizal.me/pokeapi/api/251.json')
+.then(function (response) {
+  let res = response.data;
+  let myPokeName = res.name;
+  // console.log(res);
+  let myHp = res.stats[5].base_stat;
+  let myAttack = res.stats[4].base_stat;
+  let myDefence = res.stats[3].base_stat;
+
+
+  pokeFirstName.innerHTML = `Name: ${myPokeName}`;
+  pokeHpStats.innerHTML = `PH points:              ${myHp}`;
+  pokeAttackStats.innerHTML = `Attack points:      ${myHp}`;
+  pokeDefenceStats.innerHTML = `Defence points:    ${myHp}`;
+
+
+})
+.catch(function (error) {
+  console.log(error);
+});
