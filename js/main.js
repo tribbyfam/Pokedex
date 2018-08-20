e22a167d5ff858d800eb6db7928bf8d957f3cd7c
Mac <mac@MacBook-Pro.fios-router.home>
2018-08-19 19:52:47 -0500

    tried to make abilities array work no success for now

--- js/main.js
+++ js/main.js
@@ -29,35 +29,125 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // }
 
-// api calls requesting data
+// api calls requesting data for Celebi
 
-let pokeFirstName = document.getElementById("poke-name");
-let pokeHpStats = document.getElementById("poke-hp");
-let pokeAttackStats = document.getElementById("poke-attack");
-let pokeDefenceStats = document.getElementById("poke-defence");
-let pokeAbilities = document.getElementById("poke-abilities");
+let pokeName251 = document.getElementById("poke-name");
+let pokeHpStats251 = document.getElementById("poke-hp");
+let pokeAttackStats251 = document.getElementById("poke-attack");
+let pokeDefenceStats251 = document.getElementById("poke-defence");
+let pokeAbilities251 = document.getElementById("poke-abilities");
+
+
+class trainer{
+  constructor(name, pokeName){
+    this.pokeName = pokeName
+  }
+}
+class pokemon extends trainer{
+  constructor(pokeName, ph, attack, deffend, abilities){
+    super(pokeName);
+    this.ph = ph;
+    this.attack = attack;
+    this.deffend = deffend;
+    this.abilities = abilities;
+  }
+}
+
 
 axios.get('http://fizal.me/pokeapi/api/251.json')
 .then(function (response) {
   let res = response.data;
   let myPokeName = res.name;
-  // console.log(res);
+  console.log(res);
   let myHp = res.stats[5].base_stat;
   let myAttack = res.stats[4].base_stat;
   let myDefence = res.stats[3].base_stat;
-  let myAbilities = res.abilities[0].ability.name;
-  
-  // for (let i=0; i<myAbilities.length; i++){
-  //   console.log(myAbilities[i]);
-  // }
-
-  pokeFirstName.innerHTML = `Name: ${myPokeName}`;
-  pokeHpStats.innerHTML = `${myHp}`;
-  pokeAttackStats.innerHTML = `${myHp}`;
-  pokeDefenceStats.innerHTML = `${myHp}`;
-  pokeAbilities.innerHTML = `${myAbilities}`;
+  let myAbilities = res.abilities[0].ability;
+  let newArray = [];
+  // for (let i=0; i<myAbilities.length; i++) {
+  //   newArray.push(myAbilities[i].ability.name)
+  // };
+  while(myAbilities< myAbilities.length){
+    newArray.push(myAbilities[i].ability.name)
+  }
+    
+  console.log(newArray);
+ 
+  pokeName251.innerHTML = `Name: ${myPokeName}`;
+  pokeHpStats251.innerHTML = `${myHp}`;
+  pokeAttackStats251.innerHTML = `${myAttack}`;
+  pokeDefenceStats251.innerHTML = `${myDefence}`;
+  // pokeAbilities251.innerHTML = `${myAbil[i] }`;
 
 })
 .catch(function (error) {
   console.log(error);
 });
+
+
+// api calls for Primarina
+
+// axios.get('http://fizal.me/pokeapi/api/730.json')
+// .then(function (response) {
+//   let res = response.data;
+//   let myPokeName = res.name;
+ 
+//   let myHp = res.stats[5].base_stat;
+//   let myAttack = res.stats[4].base_stat;
+//   let myDefence = res.stats[3].base_stat;
+//   let myAbilities = res.abilities[0];
+  
+//   myAbilities.forEach(function(ability){
+//     console.log(ability)
+//   });
+
+  // for (let i=0; i<myAbilities.length; i++){
+  //   let myAbil= myAbilities[i].ability.name;
+    
+  // };
+
+//   pokeName730.innerHTML = `Name: ${myPokeName}`;
+//   pokeHpStats730.innerHTML = `${myHp}`;
+//   pokeAttackStats730.innerHTML = `${myAttack}`;
+//   pokeDefenceStats730.innerHTML = `${myDefence}`;
+//   pokeAbilities730.innerHTML = `${myAbil[i].length}`;
+
+// })
+// .catch(function (error) {
+//   console.log(error);
+// });
+
+// changing stats in display template
+// document.getElementById('730').document.addEventListener('cklick', function(){
+  
+// });
+
+
+// api calls for Infernape
+
+
+// axios.get('http://fizal.me/pokeapi/api/392.json')
+// .then(function (response) {
+//   let res = response.data;
+//   let myPokeName = res.name;
+//   console.log(res);
+  // let myHp = res.stats[5].base_stat;
+  // let myAttack = res.stats[4].base_stat;
+  // let myDefence = res.stats[3].base_stat;
+  // let myAbilities = res.abilities[0];
+  
+  // for (let i=0; i<myAbilities.length; i++){
+  //   let myAbil= myAbilities[i].ability.name;
+    
+  // };
+
+  // pokeName392.innerHTML = `Name: ${myPokeName}`;
+  // pokeHpStats392.innerHTML = `${myHp}`;
+  // pokeAttackStats392.innerHTML = `${myAttack}`;
+  // pokeDefenceStats392.innerHTML = `${myDefence}`;
+  // pokeAbilities392.innerHTML = `${myAbil[i].length}`;
+
+// })
+// .catch(function (error) {
+//   console.log(error);
+// });
